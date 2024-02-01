cdc27b2caf8fcb8d815bb00a75d3750fda8f2d87
DucThanh <phoducthanh@gmail.com>
2024-02-01 11:17:45 +0700

    fix Header component in Mobile Screen

--- app/mobile/components/header/Header.tsx
+++ app/mobile/components/header/Header.tsx
@@ -3,11 +3,11 @@ import './Header.scss'
 
 function Header() {
     return ( 
-        <div className='h-16 flex items-center p-0 border-b-2 divide-solid border-gray-100'>
-            <div className='w-10 h-10 flex items-center'>
+        <div className='h-16 flex items-center justify-center border-b-2 border-solid border-gray-100'>
+            <div className='w-10 h-10 flex items-center left-1 absolute'>
                 <img src='https://ttpl.vn/assets/images/logo/logo-legalzone.png' alt='logo-legalzone'/>
             </div>
-            <span className='title text-xl font-bold ml-5 translate-x-2/3 absolute w-fit'>Thủ tục pháp luật</span>
+            <span className='flex text-xl font-bold ml-5 w-fit'>Thủ tục pháp luật</span>
         </div>
     );
 }
