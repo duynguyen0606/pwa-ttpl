98e208a325aa488ba65efd9707b581e80b4e205d
DucThanh <phoducthanh@gmail.com>
2024-02-07 10:13:22 +0700

    footer

--- app/mobile/components/footer/Footer.tsx
+++ app/mobile/components/footer/Footer.tsx
@@ -1,21 +1,29 @@
+import Link from "next/link";
+
 function Footer() {
     return ( 
-        <div className="flex items-center justify-between">
-            <div className="flex items-center">
-                <a href="#" className="mx-4">
-                    <img src="https://ttpl.vn/assets/images/icon/briefcase.png" />
-                </a>
-                <a href="#" className="mx-4">
-                    <img src="https://ttpl.vn/assets/images/icon/file-text.png" />
-                </a>
-            </div>
-            <div className="flex items-center">
-                <a href="#" className="mx-4">
-                    <img src="https://ttpl.vn/assets/images/icon/message-circle-2.png" />
-                </a>
-                <a href="#" className="mx-4">
-                    <img className="w-7 h-7" src="https://ttpl.vn/assets/images/header/User.png" />
-                </a>
+        <div className="fixed w-full h-16 bottom-0 bg-cover bg-center bg-[url(https://ttpl.vn/assets/images/Backgound-footer.png)]
+        ">
+            <a href="/mobile" className="absolute translate-x-2/4 right-1/2 z-[1] rounded-full bottom-[14px]">
+                <img src="https://ttpl.vn/assets/images/Home.png"/>
+            </a>
+            <div className="fixed bottom-0 w-full px-[15px] py-4 flex items-center justify-between">
+                <div className="flex items-center">
+                    <a href="#" className="mx-4">
+                        <img src="https://ttpl.vn/assets/images/icon/briefcase.png" />
+                    </a>
+                    <Link href="/mobile/bai-viet" className="mx-4">
+                        <img src="https://ttpl.vn/assets/images/icon/file-text.png" />
+                    </Link>
+                </div>
+                <div className="flex items-center">
+                    <a href="#" className="mx-4">
+                        <img src="https://ttpl.vn/assets/images/icon/message-circle-2.png" />
+                    </a>
+                    <a href="/mobile/dang-nhap" className="mx-4">
+                        <img className="w-7 h-7" src="https://ttpl.vn/assets/images/header/User.png" />
+                    </a>
+                </div>
             </div>
         </div>
     );
