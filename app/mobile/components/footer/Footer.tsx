73b23eb48a92626f75477b3aae53e19671368408
DucThanh <phoducthanh@gmail.com>
2024-02-14 22:30:09 +0700

    fix router Header, Footer

--- app/mobile/components/footer/Footer.tsx
+++ app/mobile/components/footer/Footer.tsx
@@ -4,25 +4,25 @@ function Footer() {
     return ( 
         <div className="fixed w-full h-16 bottom-0 bg-cover bg-center bg-[url(https://ttpl.vn/assets/images/Backgound-footer.png)]
         ">
-            <a href="/mobile" className="absolute translate-x-2/4 right-1/2 z-[1] rounded-full bottom-[14px]">
+            <Link href="/mobile" className="absolute translate-x-2/4 right-1/2 z-[1] rounded-full bottom-[14px]">
                 <img src="https://ttpl.vn/assets/images/Home.png"/>
-            </a>
+            </Link>
             <div className="fixed bottom-0 w-full px-[15px] py-4 flex items-center justify-between">
                 <div className="flex items-center">
-                    <a href="#" className="mx-4">
+                    <Link href="#" className="mx-4">
                         <img src="https://ttpl.vn/assets/images/icon/briefcase.png" />
-                    </a>
+                    </Link>
                     <Link href="/mobile/bai-viet" className="mx-4">
                         <img src="https://ttpl.vn/assets/images/icon/file-text.png" />
                     </Link>
-                </div>
+                </div>  
                 <div className="flex items-center">
-                    <a href="#" className="mx-4">
+                    <Link href="#" className="mx-4">
                         <img src="https://ttpl.vn/assets/images/icon/message-circle-2.png" />
-                    </a>
-                    <a href="/mobile/dang-nhap" className="mx-4">
+                    </Link>
+                    <Link href="/mobile/dang-nhap" className="mx-4">
                         <img className="w-7 h-7" src="https://ttpl.vn/assets/images/header/User.png" />
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
