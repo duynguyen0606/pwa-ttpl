4f68c02f31014643a545f6f515745d53cc8cdec8
duynguyen <duynguyen6601@gmail.com>
2024-02-19 09:34:07 +0700

    redux auth

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoriesReducer from './feature/categoriesSlice';
+import authReducer from './feature/authSlice';
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       categoriesState: categoriesReducer,
+      authState: authReducer,
     },
   });
 };
