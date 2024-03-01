1c900ed108113d59fa3e48fae1c9a8b4042a799a
duynguyen <duynguyen6601@gmail.com>
2024-03-01 11:47:16 +0700

    follower

--- src/components/user/ProfileFollow.tsx
+++ src/components/user/ProfileFollow.tsx
@@ -1,41 +1,83 @@
-import { ConfigProvider, Menu } from 'antd';
+import { useAppSelector } from '@/src/redux/hooks';
+import { Avatar, Button, ConfigProvider, Menu } from 'antd';
+import { useState } from 'react';
 
 const dataNavs = [
   {
     label: 'Người theo dõi',
-    key: 1,
+    key: 'follower',
   },
   {
     label: 'Đang theo dõi',
-    key: 2,
+    key: 'watching',
   },
 ];
 function ProfileFollow() {
+  const [typeNav, setTypeNav] = useState('follower');
+  const { listFollower, listWatching } = useAppSelector(
+    (state) => state.userState
+  );
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: 'var(--primary-color)',
-        },
-        components: {
-          Menu: {
-            horizontalItemSelectedColor: 'var(--primary-color)',
+    <div>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: 'var(--primary-color)',
           },
-        },
-      }}
-    >
-      <Menu
-        mode='horizontal'
-        defaultSelectedKeys={['2']}
-        items={dataNavs.map((item) => {
-          return {
-            key: item.key,
-            label: item.label,
-          };
-        })}
-        style={{ justifyContent: 'center', fontSize: 18 }}
-      />
-    </ConfigProvider>
+          components: {
+            Menu: {
+              horizontalItemSelectedColor: 'var(--primary-color)',
+            },
+          },
+        }}
+      >
+        <Menu
+          mode='horizontal'
+          defaultSelectedKeys={[typeNav]}
+          items={dataNavs.map((item) => {
+            return {
+              key: item.key,
+              label: item.label,
+            };
+          })}
+          style={{ justifyContent: 'center', fontSize: 18 }}
+          onSelect={({ item, key }) => setTypeNav(key)}
+        />
+      </ConfigProvider>
+      <div className='bg-white'>
+        <div className='w-1/2 m-auto p-4'>
+          {typeNav === 'follower' ? (
+            <div className='grid grid-cols-2 gap-4 border '>
+              {listFollower.map((item) => (
+                <div className='flex items-center justify-between p-2 rounded-lg border border-gray-300'>
+                  <div className='flex items-center gap-2'>
+                    <Avatar src={item?.avatar_user_answer} size='large' />
+                    <div>{item?.name_user_answer}</div>
+                  </div>
+                  <Button disabled>Đang theo dõi</Button>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <>
+              {listWatching?.length && (
+                <div className='grid grid-cols-2 gap-4'>
+                  {listWatching.map((item) => (
+                    <div className='flex items-center justify-between p-2 rounded-lg border border-gray-300'>
+                      <div className='flex items-center gap-2'>
+                        <Avatar src={item?.avatar_user_answer} size='large' />
+                        <div>{item?.name_user_answer}</div>
+                      </div>
+                      <Button disabled>Đang theo dõi</Button>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
