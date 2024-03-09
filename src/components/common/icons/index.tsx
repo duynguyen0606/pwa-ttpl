b22f79ffc059b27219ec9012a37bdcaca1c51d2f
duynguyen <duynguyen6601@gmail.com>
2024-03-09 11:38:13 +0700

    like post

--- src/components/common/icons/index.tsx
+++ src/components/common/icons/index.tsx
@@ -1,5 +1,6 @@
-export { default as CrownIcon } from "./CrownIcon";
-export { default as LawyerIcon } from "./LawyerIcon";
-export { default as EnterpriseIcon } from "./EnterpriseIcon";
-export { default as PersonalIcon } from "./PersonalIcon";
-export { default as StateCadresIcon } from "./StateCadresIcon";
+export { default as CrownIcon } from './CrownIcon';
+export { default as LawyerIcon } from './LawyerIcon';
+export { default as EnterpriseIcon } from './EnterpriseIcon';
+export { default as PersonalIcon } from './PersonalIcon';
+export { default as StateCadresIcon } from './StateCadresIcon';
+export { default as LikeIcon } from './LikeIcon';
