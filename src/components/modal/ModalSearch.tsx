711158740f0113128345cd116f7a43c10440d513
duynguyen <duynguyen6601@gmail.com>
2024-03-13 09:09:01 +0700

    search page

--- src/components/modal/ModalSearch.tsx
+++ src/components/modal/ModalSearch.tsx
@@ -1,16 +1,28 @@
 import { Modal, ModalProps } from 'antd';
 import Search from 'antd/es/input/Search';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 function ModalSearch(props: ModalProps) {
   const { open, onOk, onCancel } = props;
+  const router = useRouter();
+  const [value, setValue] = useState('');
   return (
     <Modal
       title='Tìm kiếm thủ tục pháp luật'
       open={open}
-      onOk={onOk}
+      onOk={() => {
+        router.push(`/tim-kiem?search=${value}`);
+        onCancel;
+      }}
       onCancel={onCancel}
     >
-      <Search placeholder='Tìm kiếm ....' allowClear size='large' />
+      <Search
+        onChange={(e) => setValue(e.target.value)}
+        placeholder='Tìm kiếm ....'
+        allowClear
+        size='large'
+      />
     </Modal>
   );
 }
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,97 +1,87 @@
-"use client";
+'use client';
 
-import { useEffect, useMemo, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { apiSearchGlobal } from "@/src/api/search";
+import { useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { apiSearchGlobal } from '@/src/api/search';
 
-import SearchSideBar from "./SearchSideBar";
-import SearchPostResult from "./SearchPostResult";
-import SearchProcedureResult from "./SearchProcedureResult";
-import SearchQuestionResult from "./SearchQuestionResult";
-import SearchLawyerResult from "./SearchLawyerResult";
-import SearchGroupResult from "./SearchGroupResult";
+import SearchSideBar from './SearchSideBar';
+import SearchPostResult from './SearchPostResult';
+import SearchProcedureResult from './SearchProcedureResult';
+import SearchQuestionResult from './SearchQuestionResult';
+import SearchLawyerResult from './SearchLawyerResult';
+import SearchGroupResult from './SearchGroupResult';
 
 enum TypeTabs {
-    ALL = "list",
-    POST = "list_post_favorite",
-    PROCEDURE = "list_data_thutuc",
-    QUESTION = "list_cauhoi",
-    LAWYER = "list_data_luatsu",
-    GROUP = "list_data_moinguoi",
+  ALL = 'list',
+  POST = 'list_post_favorite',
+  PROCEDURE = 'list_data_thutuc',
+  QUESTION = 'list_cauhoi',
+  LAWYER = 'list_data_luatsu',
+  GROUP = 'list_data_moinguoi',
 }
 
 function SearchPage() {
-    const searchParams = useSearchParams();
-    const searchValue = searchParams.get("search");
-    const [tabActive, setTabActive] = useState(TypeTabs.ALL);
-    const [data, setData] = useState<any>(null);
+  const searchParams = useSearchParams();
+  const searchValue = searchParams.get('search');
+  const [tabActive, setTabActive] = useState(TypeTabs.ALL);
+  const [data, setData] = useState<any>(null);
 
-    useEffect(() => {
-        if (searchParams) {
-            (async () => {
-                const dataRes = await apiSearchGlobal({
-                    search_procedure: searchValue as string,
-                });
-                if (dataRes?.status) {
-                    setData(dataRes.data);
-                }
-            })();
+  useEffect(() => {
+    if (searchParams) {
+      (async () => {
+        const dataRes = await apiSearchGlobal({
+          search_procedure: searchValue as string,
+        });
+        if (dataRes?.status) {
+          setData(dataRes.data);
         }
-    }, [searchParams]);
+      })();
+    }
+  }, [searchValue]);
 
-    const renderContent = useMemo(() => {
-        if (data) {
-            switch (tabActive) {
-                case TypeTabs.ALL:
-                    return <div>all</div>;
-                case TypeTabs.POST:
-                    return (
-                        <SearchPostResult
-                            listPost={data[TypeTabs.POST].result}
-                        />
-                    );
-                case TypeTabs.PROCEDURE:
-                    return (
-                        <SearchProcedureResult
-                            listProcedure={data[TypeTabs.PROCEDURE].result}
-                        />
-                    );
-                case TypeTabs.QUESTION:
-                    return (
-                        <SearchQuestionResult
-                            listQuestion={data[TypeTabs.QUESTION].result}
-                        />
-                    );
-                case TypeTabs.LAWYER:
-                    return (
-                        <SearchLawyerResult
-                            listLawyer={data[TypeTabs.LAWYER].result}
-                        />
-                    );
-                case TypeTabs.GROUP:
-                    return (
-                        <SearchGroupResult
-                            listGroup={data[TypeTabs.GROUP].result}
-                        />
-                    );
-            }
-        }
-    }, [tabActive, searchParams, data]);
+  const renderContent = useMemo(() => {
+    if (data) {
+      switch (tabActive) {
+        case TypeTabs.ALL:
+          return <div>all</div>;
+        case TypeTabs.POST:
+          return <SearchPostResult listPost={data[TypeTabs.POST].result} />;
+        case TypeTabs.PROCEDURE:
+          return (
+            <SearchProcedureResult
+              listProcedure={data[TypeTabs.PROCEDURE].result}
+            />
+          );
+        case TypeTabs.QUESTION:
+          return (
+            <SearchQuestionResult
+              listQuestion={data[TypeTabs.QUESTION].result}
+            />
+          );
+        case TypeTabs.LAWYER:
+          return (
+            <SearchLawyerResult listLawyer={data[TypeTabs.LAWYER].result} />
+          );
+        case TypeTabs.GROUP:
+          return <SearchGroupResult listGroup={data[TypeTabs.GROUP].result} />;
+      }
+    }
+  }, [tabActive, searchValue, data]);
 
-    return (
-        <div
-            className="flex justify-between flex-wrap"
-            style={{ maxWidth: 1460, margin: "auto" }}
-        >
-            <div style={{ width: 400 }}>
-                <SearchSideBar
-                    tabActive={tabActive}
-                    onSetTabActive={(typeTab) => setTabActive(typeTab)}
-                />
-            </div>
-            <div style={{ width: 650 }}>{renderContent}</div>
-        </div>
-    );
+  return (
+    <div
+      className='flex justify-between flex-wrap'
+      style={{ maxWidth: 1460, margin: 'auto' }}
+    >
+      <div style={{ width: 400 }}>
+        <SearchSideBar
+          tabActive={tabActive}
+          onSetTabActive={(typeTab) => setTabActive(typeTab)}
+        />
+      </div>
+      <div style={{ width: 650 }}>{renderContent}</div>
+    </div>
+  );
 }
 
 export default SearchPage;
