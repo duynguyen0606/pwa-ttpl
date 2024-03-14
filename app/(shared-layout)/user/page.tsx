adbc552b47c5837bc3c3cf6d0ae962d9fd09ab47
DucThanh <phoducthanh@gmail.com>
2024-03-14 10:40:54 +0700

    classify profileScreen with type

--- app/(shared-layout)/user/page.tsx
+++ app/(shared-layout)/user/page.tsx
@@ -1,12 +1,27 @@
-'use client';
+"use client";
 
-import UserProfile from '@/src/components/user/personal';
-// import UserProfile from '@/src/components/user';
-import { usePageAuth } from '@/src/utils/hook';
+import { usePageAuth } from "@/src/utils/hook";
+import { useAppSelector } from "@/src/redux/hooks";
+import LawyerProfile from "@/src/components/user/lawyer";
+import UserProfile from "@/src/components/user/personal";
+import LawEnterprise from "@/src/components/user/enterprise/law-enterprise";
+import LawCompany from "@/src/components/user/enterprise/law-company";
 
 function Index() {
-  usePageAuth();
-  return <UserProfile />;
+    usePageAuth();
+    const { user } = useAppSelector((state) => state.authState);
+    switch (user?.user_type) {
+        case "personal":
+            return <UserProfile />;
+        case "lawyer":
+            return <LawyerProfile />;
+        case "state_cadres":
+            return <UserProfile />;
+        case "enterprise":
+            return <LawEnterprise />;
+        case "company":
+            return <LawCompany />;
+    }
 }
 
 export default Index;
