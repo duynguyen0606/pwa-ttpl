0d599dc5e722527801070dafc752698177cbe060
duynguyen <duynguyen6601@gmail.com>
2024-03-15 09:29:16 +0700

    fix cache

--- next.config.js
+++ next.config.js
@@ -5,49 +5,12 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  cacheOnFrontEndNav: true,
+  // cacheOnFrontEndNav: true,
   runtimeCaching,
-  // runtimeCaching,
-  // workboxOptions: {
-  //   disableDevLogs: true,
-  // },
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  // scope: '/app',
-  // sw: 'service-worker.js',
-  //...
+  buildExcludes: [/app-build-manifest.json$/],
 });
 
 module.exports = withPWA({
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //     // https://github.com/webpack-contrib/mini-css-extract-plugin#recommended
-  //     // For production builds it's recommended to extract the CSS from your bundle being able to use parallel loading of CSS/JS resources later on.
-  //     // For development mode, using style-loader because it injects CSS into the DOM using multiple <style></style> and works faster.
-  //     if (!dev) {
-  //         config.plugins.push(new MiniCssExtractPlugin({
-  //             filename: 'static/chunks/[name].[fullhash].css',
-  //             ignoreOrder: true
-  //         }));
-  //     }
-  //     config.module.rules.push(
-  //         {
-  //             test: /\.(sa|sc|c)ss$/,
-  //             use: [
-  //                 isServer ? { loader: 'file-loader' } : (dev ? { loader: 'style-loader' } : { loader: MiniCssExtractPlugin.loader }),
-  //                 { loader: 'css-loader' },
-  //                 { loader: 'sass-loader' }
-  //             ]
-  //         }
-  //     );
-  //     return config;
-  // },
-  // experimental: {
-  //   appDir: true,
-  // },
-  // lessLoaderOptions: {
-  //   javascriptEnabled: true,
-  //   importLoaders: true,
-  // },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
@@ -60,9 +23,5 @@ module.exports = withPWA({
         pathname: '**',
       },
     ],
-  },
-  corePlugins: {
-    // due to https://github.com/tailwindlabs/tailwindcss/issues/6602 - buttons disappear
-    preflight: false,
   },
 });
