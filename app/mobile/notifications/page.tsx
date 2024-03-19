4041473bf9532283819d09c72da2e6acdc030bac
DucThanh <phoducthanh@gmail.com>
2024-03-19 19:18:50 +0700

    notification page mobile

--- app/mobile/notifications/page.tsx
+++ app/mobile/notifications/page.tsx
@@ -1,11 +1,40 @@
-import CanBackLayout from '@/src/components/layout/mobile/CanBackLayout';
+'use client'
+
+import { Avatar } from "antd";
+import { useAppSelector } from "@/src/redux/hooks";
+import CanBackLayout from "@/src/components/layout/mobile/CanBackLayout";
 
 function Index() {
-  return (
-    <CanBackLayout back='/mobile' title='Thông báo'>
-      <div className='p-4'>No notification found.</div>
-    </CanBackLayout>
-  );
+    const { listNotification } = useAppSelector((state) => state.userState);
+    return (
+        <CanBackLayout back="/mobile" title="Thông báo">
+            <div className="p-4">
+                {listNotification?.length > 0 ? (
+                    <>
+                        {listNotification?.map((item) => (
+                            <div
+                                key={item.avatar_create_by}
+                                className="flex items-start gap-2 py-2"
+                            >
+                                <Avatar
+                                    src={item.avatar_create_by}
+                                    size="large"
+                                />
+                                <div>
+                                    <span className="font-semibold text-lg">
+                                        {item.name_create_by}
+                                    </span>{" "}
+                                    <span>{item.name_type}</span>
+                                </div>
+                            </div>
+                        ))}
+                    </>
+                ) : (
+                    <p>No notification found.</p>
+                )}
+            </div>
+        </CanBackLayout>
+    );
 }
 
 export default Index;
